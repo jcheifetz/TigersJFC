fc55b83555b82b96c9c0864d4e0bca179fd04ca0
Jonathan Cheifetz <jonathan.cheifetz@gmail.com>
2019-04-25 09:13:23 +0100

    Init

---
+++ Website.Logic/Website.logic/Controllers/Partial Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using DevTrends.MvcDonutCaching;
+using System;
+
+
+namespace Website.logic.Controllers.Partial_Controllers
+{
+    class MenuController : SurfaceController
+    {
+
+        private string GetPartialViewPath(string name)
+        {
+            return $"~/Views/Partials/PageSnippets/{name}.cshtml";
+        }
+
+        [ChildActionOnly]
+        [DonutOutputCache(CacheProfile = "OneMonth")]
+        public ActionResult NavFooterUpper(int startNodeId)
+        {
+            NavFooterModel model = new NavFooterModel(CurrentPage);
+            model.StartNodeId = startNodeId;
+            return PartialView(GetPartialViewPath("NavFooterUpper"), model);
+        }
+    }
+}
---
+++ Website.Logic/Website.logic/Models/Partial Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+
+namespace Website.logic.Models.Partial_Models
+{
+    class Menu : ContentModel
+    {
+        public AccountLinks(IPublishedContent content) : base(content)
+        {
+        }
+    
+    }
+}
